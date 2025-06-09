e4d3964b483986560f9ff6b4a0c5650c5f369f5f
kajjtek <kaj.plw24@gmail.com>
2025-06-09 18:54:46 +0200

    better csv

--- SEM4/AiSD/list5/Heaps/Tests/main.cpp
+++ SEM4/AiSD/list5/Heaps/Tests/main.cpp
@@ -9,46 +9,49 @@ void runExperiment(int n, int experimentId) {
     BinomialHeap H1, H2;
 
     std::srand(std::time(nullptr) + experimentId);
+
+    std::vector<int> comparisonsLog;  // Store comparisons in order
+
+    // Insert n elements into H1, log comparisons each insert
     for (int i = 0; i < n; ++i) {
+        H1.resetComparisons();
         H1.insert(std::rand());
-        H2.insert(std::rand());
+        comparisonsLog.push_back(H1.getComparisons());
     }
 
+    // Insert n elements into H2, log comparisons each insert
+    for (int i = 0; i < n; ++i) {
+        H2.resetComparisons();
+        H2.insert(std::rand());
+        comparisonsLog.push_back(H2.getComparisons());
+    }
+
+    // Heap-union operation (merge)
+    H1.resetComparisons();
     H1.merge(H2);
+    comparisonsLog.push_back(H1.getComparisons());
 
-    std::vector<int> extracted;
-    std::vector<int> comparisonsPerExtract;
-
+    // Now 2*n extractMin operations with comparisons logged
     for (int i = 0; i < 2 * n; ++i) {
         H1.resetComparisons();
-        int min = H1.extractMin();
-        int comps = H1.getComparisons();
-
-        if (min == INT_MIN) {
+        int minVal = H1.extractMin();
+        if (minVal == INT_MIN) {
             std::cerr << "Error: extracted INT_MIN\n";
             return;
         }
-
-        comparisonsPerExtract.push_back(comps);
-        extracted.push_back(min);
+        comparisonsLog.push_back(H1.getComparisons());
     }
 
-    // Check if sorted
-    for (size_t i = 1; i < extracted.size(); ++i) {
-        if (extracted[i - 1] > extracted[i]) {
-            std::cerr << "Error: Extracted sequence is not sorted.\n";
-            return;
-        }
-    }
-
-    // Save to file
+    // Save to single CSV file
     std::ofstream file("experiment_" + std::to_string(experimentId) + ".csv");
-    for (size_t i = 0; i < comparisonsPerExtract.size(); ++i) {
-        file << i + 1 << "," << comparisonsPerExtract[i] << "\n";
+    for (size_t i = 0; i < comparisonsLog.size(); ++i) {
+        file << (i + 1) << "," << comparisonsLog[i] << "\n";
     }
     file.close();
+
     std::cout << "Experiment " << experimentId << " done.\n";
 }
+
 
 void runAverageCostExperiment() {
     std::ofstream file("average_cost.csv");
